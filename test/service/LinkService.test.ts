330208ac0d53af84d5f7c30ea0132fbc0d7ab623
fernando <fboucquez@gmail.com>
2020-09-29 18:52:49 -0300

    Fixed test

--- test/service/LinkService.test.ts
+++ test/service/LinkService.test.ts
@@ -1,15 +1,20 @@
 import 'mocha';
-import { BootstrapService } from '../../src/service';
+import { BootstrapService, ConfigService, Preset } from '../../src/service';
 import { LinkService } from '../../src/service/LinkService';
 import { expect } from '@oclif/test';
 
 describe('LinkService', () => {
     it('LinkService testnet', async () => {
         const params = {
+            ...ConfigService.defaultParams,
             ...LinkService.defaultParams,
             target: 'target/testnet-dual',
+            reset: true,
+            preset: Preset.testnet,
+            assembly: 'dual',
         };
         try {
+            await new BootstrapService('.').config(params);
             await new BootstrapService('.').link(params);
         } catch (e) {
             expect(e.message.indexOf('ECONNREFUSED'), `Not a connection error: ${e.message}`).to.be.greaterThan(-1);
