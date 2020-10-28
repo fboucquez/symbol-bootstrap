92d6e6eb0d4d048cc4e07479e2118abf6dd87f50
fboucquez <fboucquez@gmail.com>
2020-10-28 12:44:00 -0700

    Added license headers (#59)
    Added license headers
    Added index.ts generator command

---
+++ src/logger/index.ts
@@ -0,0 +1,5 @@
+// created from 'create-ts-index'
+
+export * from './LogType';
+export * from './Logger';
+export * from './LoggerFactory';
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,8 +1,14 @@
+// created from 'create-ts-index'
+
+export * from './BootstrapService';
 export * from './BootstrapUtils';
 export * from './CertificateService';
 export * from './ComposeService';
 export * from './ConfigService';
+export * from './ForgeCertificateService';
+export * from './LinkService';
 export * from './NemgenService';
+export * from './PortService';
+export * from './ReportService';
 export * from './RunService';
-export * from './BootstrapService';
 export * from './VotingService';
