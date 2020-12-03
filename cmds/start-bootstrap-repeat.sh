a4742824e85201d967a228319b009b618d702a74
fernando <fboucquez@gmail.com>
2020-12-03 08:38:36 -0300

    Fixed e2e Added lodash

--- cmds/start-bootstrap-repeat.sh
+++ cmds/start-bootstrap-repeat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-symbol-bootstrap start -p bootstrap -c test/repeat_preset.yml -t target/bootstrap-repeat $1 $2 $3
+symbol-bootstrap start -p bootstrap -c test/repeat_preset.yml -t target/bootstrap-repeat $1
