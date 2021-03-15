32f97f48718b850ec536b3bbe8ebdebe0ce4692e
fernando <fboucquez@gmail.com>
2021-03-15 06:01:44 -0300

    Updated sink addresses

--- cmds/download-mainnet-seed.sh
+++ cmds/download-mainnet-seed.sh
@@ -11,3 +11,12 @@ rm -rf privateSeeds
 7z x nemesis.7z "-p$3"
 mv nemesis privateSeeds
 mv resources ./privateSeeds
+
+
+cd privateSeeds
+zip -r ./seed.zip ./seed
+cd ..
+rm -rf ../symbol-node-registration-cli/mainnet/seed
+rm -rf ../symbol-node-registration-cli/mainnet/resources
+
+cp -r ./privateSeeds/* ../symbol-node-registration-cli/mainnet
