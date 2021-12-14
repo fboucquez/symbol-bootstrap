f0b3117ce2504944e1868ef790f207998fc96a40
fboucquez <fboucquez@gmail.com>
2021-12-14 14:00:18 -0300

    fix: removed unused files after nemesis block is generated. (#339)
    using --useTemporaryCacheDatabase option

--- src/service/NemgenService.ts
+++ src/service/NemgenService.ts
@@ -36,7 +36,7 @@ export class NemgenService {
         }
 
         const nemesisWorkingDir = BootstrapUtils.getTargetNemesisFolder(target, true);
-        const nemesisSeedFolder = join(nemesisWorkingDir, `seed`, `${networkIdentifier}`, `0000`);
+        const nemesisSeedFolder = join(nemesisWorkingDir, `seed`, networkIdentifier, `0000`);
         await BootstrapUtils.mkdir(nemesisSeedFolder);
         await promises.copyFile(join(BootstrapUtils.ROOT_FOLDER, `config`, `hashes.dat`), join(nemesisSeedFolder, `hashes.dat`));
         const name = presetData.nodes[0].name;
@@ -49,25 +49,37 @@ export class NemgenService {
             `${presetData.catapultAppFolder}/bin/catapult.tools.nemgen`,
             '--resources=/server-config',
             '--nemesisProperties=./server-config/block-properties-file.properties',
+            '--useTemporaryCacheDatabase',
         ];
 
         const binds = [`${serverConfigWorkingDir}:/server-config`, `${nemesisWorkingDir}:/nemesis`];
 
         const userId = await BootstrapUtils.resolveDockerUserFromParam(this.logger, this.params.user);
-        const { stdout, stderr } = await BootstrapUtils.runImageUsingExec(this.logger, {
-            catapultAppFolder: presetData.catapultAppFolder,
-            image: symbolServerImage,
-            userId: userId,
-            workdir: '/nemesis',
-            cmds: cmd,
-            binds: binds,
-        });
-
-        if (stdout.indexOf('<error>') > -1) {
-            this.logger.info(stdout);
-            this.logger.error(stderr);
+        let stdout: string;
+        let stderr: string;
+        let message: string | undefined;
+        let failed: boolean;
+        try {
+            ({ stdout, stderr } = await BootstrapUtils.runImageUsingExec(this.logger, {
+                catapultAppFolder: presetData.catapultAppFolder,
+                image: symbolServerImage,
+                userId: userId,
+                workdir: '/nemesis',
+                cmds: cmd,
+                binds: binds,
+            }));
+            failed = stdout.indexOf('<error>') > -1;
+        } catch (e) {
+            failed = true;
+            ({ stdout, stderr, message } = e);
+        }
+        if (failed) {
+            if (message) this.logger.error(message);
+            if (stdout) this.logger.info(stdout);
+            if (stderr) this.logger.error(stderr);
             throw new Error('Nemgen failed. Check the logs!');
         }
+        BootstrapUtils.deleteFolder(this.logger, join(nemesisWorkingDir, `seed`, networkIdentifier));
         this.logger.info('Nemgen executed!!!!');
     }
 }
